2ef6c3469a8308cc53b7aad8fe43e4242f902962
Kaelyn <kaelyneleanorsun@gmail.com>
2026-03-23 21:29:03 +0800

    Resolved previous conflicts to match remote version

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,3 @@
-// import 'package:flutter/material.dart';
-/*
-class Home extends StatelessWidget{
-  final String studentID;
-  const Home({super.key, required this.studentID});
-
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: const Text('Welcome to My NTHU Life')),
-      body: Center(child: Text('Hello Student ID: $studentID')),
-    );
-  }
-}
-*/
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
